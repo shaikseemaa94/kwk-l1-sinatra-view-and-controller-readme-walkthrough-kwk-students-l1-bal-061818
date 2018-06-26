dec3b558cec8087c1c36cdb4b4255a857993fbfb
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-26 18:34:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,12 +9,15 @@ class App < Sinatra::Base
     original_string = params["string"]
     @reversed_string = original_string.reverse
 
-    
+
       erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+
+    erb :friends 
 
   end
 end
